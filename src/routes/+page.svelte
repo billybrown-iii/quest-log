6084583c3ceafc0730897d73a13ce03e80a6ecbe
Billy Brown III <link8495@gmail.com>
2024-07-20 17:30:59 -0700

    test PR

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,14 @@
     Blue text!
 </div>
 <div class='component-scoped-class'>
-    Cyan text!
+    Lime text!
 </div>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <style lang='scss'>
-    $componentScopedScssVariable: cyan;
+    $componentScopedScssVariable: lime;
 
     .component-scoped-class {
         color: $componentScopedScssVariable;
