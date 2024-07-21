26578348919f498b57b57e4b25072ea710ea3c30
Billy Brown III <link8495@gmail.com>
2024-07-20 22:22:44 -0700

    add dynamic route

---
+++ src/routes/campaign/[slug]/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { onMount } from 'svelte';
+    export let data;
+
+    let value = 'loading...';
+
+    // if you weant to show the page instantly
+    // then update it with the fetched data
+    // then use this approach:
+    onMount(() => {
+        setTimeout(() => {
+            value = `fetched data for slug:  ${data.slug}`
+        }, 1000)
+    })
+</script>
+
+{#if value}
+<div>Here is data:</div>
+<div>{JSON.stringify(value)}</div>
+{:else}
+<div>Waiting on data</div>
+{/if}
+
+<a class="button" href="/">Click me to go back home</a>
+
